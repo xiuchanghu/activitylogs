b820db4feecfe0dbe200b15db0aa4f98b9a4aac2
huxiuchang <hu.xiuchang@icntv.tv>
2015-09-21 16:44:14 +0800

    first commit

---
+++ src/ActivityLogServiceProvider.php
@@ -0,0 +1,46 @@
+<?php namespace Xiuchanghu\ActivityLogs;
+
+use Illuminate\Support\ServiceProvider;
+
+class ActivityLogsServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->publishes([
+			__DIR__.'/config/log.php' => config_path('log.php'),
+		]);
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return [];
+	}
+
+}
